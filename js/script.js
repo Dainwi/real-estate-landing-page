bdbdca0b14a55bc78ec689469a5784195aa2e34d
Dainwi Kumar <danwichoudhary@gmail.com>
2024-03-03 17:44:32 +0530

    go to top button added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Navbar
 document.getElementById('menu-toggle').addEventListener('click', function () {
     var mobileMenu = document.getElementById('mobile-menu');
     if (mobileMenu.style.display === 'block') {
@@ -7,7 +8,7 @@ document.getElementById('menu-toggle').addEventListener('click', function () {
     }
 });
 
-
+// contact us form modal 
 const openModalButton = document.getElementById('openModal');
 const closeModalButton = document.getElementById('closeModal');
 const modal = document.getElementById('modal');
@@ -24,4 +25,22 @@ closeModalButton.addEventListener('click', () => {
     modal.style.display = 'none';
     modal.classList.remove('fade-out');
   }, 500); // Change 500 to match animation duration
+});
+
+// got to top button
+const goToTopBtn = document.getElementById("goToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        goToTopBtn.style.display = "block";
+    } else {
+        goToTopBtn.style.display = "none";
+    }
+};
+
+// When the user clicks on the button, scroll to the top of the document
+goToTopBtn.addEventListener("click", function() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
 });
