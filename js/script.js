636f43243b65a612ac4ace0c2e9db2e80a1056af
Dainwi Kumar <danwichoudhary@gmail.com>
2024-03-03 17:05:52 +0530

    added some improvements

--- js/script.js
+++ js/script.js
@@ -6,3 +6,22 @@ document.getElementById('menu-toggle').addEventListener('click', function () {
         mobileMenu.style.display = 'block';
     }
 });
+
+
+const openModalButton = document.getElementById('openModal');
+const closeModalButton = document.getElementById('closeModal');
+const modal = document.getElementById('modal');
+
+openModalButton.addEventListener('click', () => {
+  modal.style.display = 'block';
+  modal.classList.add('fade-in');
+});
+
+closeModalButton.addEventListener('click', () => {
+  modal.classList.remove('fade-in');
+  modal.classList.add('fade-out');
+  setTimeout(() => {
+    modal.style.display = 'none';
+    modal.classList.remove('fade-out');
+  }, 500); // Change 500 to match animation duration
+});
